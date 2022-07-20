5667bebc9b2c0df950d941584cfa04bbc0546b5a
jojohnsonATVI <109410385+jojohnsonATVI@users.noreply.github.com>
2022-07-20 12:53:53 -0500

    updating Challenge04
    adding proper input validation to this problem

--- Challenge04/Challenge04/Challenge04.cpp
+++ Challenge04/Challenge04/Challenge04.cpp
@@ -2,24 +2,43 @@
 
 #include <iostream>
 
+
+
 int main()
 {
+    
     std::cout << "Enter a positive number\n";
     int input;
     std::cin >> input;
-    if (input <= 0) {
-        std::cout << "A non positive number was entered";
-        return 0;
-    }
-    else {
-        std::cout << input << " Has the following products: ";
-        //since there is no possible factor between half of the input and the input itself. Really, we could make this more efficient for very large numbers if we set it to input/n and set n to 2, and change it to 3 if we get past 2 in our count, and set n to 4 if we pass 3 in our loop without 
-        for (int i = 1; i <= input / 2; i++) {
-            if (input % i == 0) {
-                std::cout << i << " ";
-           }
+    while (true) {
+        if (std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(99, '\n');
+            std::cout << "A number wasn't entered. Please enter a positive number\n";
+            std::cin >> input;
         }
-            //necessary addition since we're doing the half the input trick up above^^
-            std::cout << input;
+        else if (input < 1) {
+            std::cout << "A non positive number was entered. Please enter a positive number\n";
+            std::cin >> input;
+        }
+        else break;
+        
+       
+
     }
+    std::cout << input << " Has the following products: ";
+    /* Since there is no possible factor between half of inputand input, we can skip the last half of checks. 
+    *  This could be sped up even more if we were to run the loop from 1 to sqrt of input and add the corresponding factor 
+    *  greater than the sqrt(n) to a stack and pop them out after finishing the loop. Of course we'd have to add a check for
+    *  if the sqrt(input) is one of the factors before popping the stack so we don't end up missing or duplicatibg the sqrt result.
+    *  I'm choosing not to implement this since it's unclear what the program's goals are for memory/speed O(n).
+    */
+
+        for (int i = 1; i <= input / 2; i++) {
+        if (input % i == 0) {
+            std::cout << i << " ";
+        }
+    }
+    //necessary addition since we're doing the half the input trick up above^^
+    std::cout << input;
 }
