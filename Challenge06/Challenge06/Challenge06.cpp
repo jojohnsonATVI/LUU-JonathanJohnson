3f8fdae974e237f84281d912fffb6d439c1661b1
jojohnsonATVI <109410385+jojohnsonATVI@users.noreply.github.com>
2022-07-20 14:32:54 -0500

    Update Challenge06.cpp

--- Challenge06/Challenge06/Challenge06.cpp
+++ Challenge06/Challenge06/Challenge06.cpp
@@ -1,9 +1,18 @@
+#include<iostream>
 
-
-#include <iostream>
+int BiggestInArray(int intArray[], int length) {
+    int biggest = intArray[0];
+    for (int i = 1; i < length; i++) {
+        if (intArray[i] > biggest) {
+            biggest = intArray[i];
+        }
+    }
+    return biggest;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int myArray[] = { -64, 40, 12658, 12657, 12659, -13000, 4, 84 };
+    std::cout << BiggestInArray(myArray, 8);
 }
 
