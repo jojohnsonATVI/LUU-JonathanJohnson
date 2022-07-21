bb5109d2c86d4b9232a811295b49821d0f9cafc9
jojohnsonATVI <109410385+jojohnsonATVI@users.noreply.github.com>
2022-07-21 16:37:41 -0500

    Update Challenge04.cpp
    Implemented the more time-efficient algorithm (square root method). included in comments is the test / results used to see time efficiency when introducing a set of large numbers.

--- Challenge04/Challenge04/Challenge04.cpp
+++ Challenge04/Challenge04/Challenge04.cpp
@@ -2,11 +2,9 @@
 
 #include <iostream>
 #include <stack>
+#include <time.h>
 
-
-int main()
-{
-    
+int getGoodInt() {
     std::cout << "Enter a positive number\n";
     int input;
     std::cin >> input;
@@ -21,11 +19,10 @@ int main()
             std::cout << "A non positive number was entered. Please enter a positive number\n";
             std::cin >> input;
         }
-        else break;
-        
-       
-
+        else return input;
     }
+}
+void PrintFactors(int input) {
     std::cout << input << " Has the following factors: ";
     std::stack<int> stack;
     int sqrt = std::sqrt(input);
@@ -42,5 +39,64 @@ int main()
         std::cout << stack.top() << ' ';
         stack.pop();
     }
+    std::cout << '\n';
+}
+void PrintFactorsHalf(int input) {
+    std::cout << input << " Has the following factors: ";
+    int half = input / 2;
+    for (int i = 1; i <= half; i++) {
+        if (input % i == 0) {
+            std::cout << i << ' ';
+        }
+    }
+    std::cout << input << '\n';
+}
+void PrintFactorsFull(int input) {
+    std::cout << input << " Has the following factors: ";
+    for (int i = 1; i <= input; i++) {
+        if (input % i == 0) {
+            std::cout << i << ' ';
+        }
+    }
+    std::cout << '\n';
+}
+
+
+
+
+int main()
+{
+   /*
+    *clock_t tStart = clock();
+    *for (int i = 1999999900; i < 2000000000; i++) {
+    *    PrintFactors(i);
+    *}
+    *
+    *clock_t tStart2 = clock();
+    *for (int i = 1999999900; i < 2000000000; i++) {
+    *    PrintFactorsHalf(i);
+    *}
+    *
+    *clock_t tStart3 = clock();
+    *for (int i = 1999999900; i < 2000000000; i++) {
+    *    PrintFactorsFull(i);
+    *}
+    *printf("Time taken for Sqrt route: %.2fs\n", (double)(tStart2 - tStart) / CLOCKS_PER_SEC);
+    *printf("Time taken for half route: %.2fs\n", (double)(tStart3 - tStart2) / CLOCKS_PER_SEC);
+    *printf("Time taken for full route: %.2fs\n", (double)(clock() - tStart3) / CLOCKS_PER_SEC);
+    *
+    *return 0;
+    * 
+    * 
+    * Results:
+    * 
+    * Time taken for Sqrt route: 1.56s
+    * Time taken for half route: 134.25s
+    * Time taken for full route: 275.81s
+    */
+
+    PrintFactors(getGoodInt());
+
+   
 
 }
